6636ed543c15374295498a6698bca0df2cbc0272
Gabriel <gabegroenendaal@gmail.com>
2022-07-22 01:04:26 -0400

    Fixing the splash page for further styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 import { Universe } from "./scripts/universe.js";
-
+import { Howl, Howler } from 'howler';
 window.count = 0;
 window.board_offset = 16;
 setInterval(function () { window.count++ }, 10);
 
 document.addEventListener("DOMContentLoaded", () => {
+      //document.getElementById('splash-page-container').classList.replace('hidden', 'visible');
+
       document.getElementById('start-button').classList.replace('hidden', 'visible');
       document.getElementById('title').classList.replace('hidden', 'visible');
       document.getElementById('sound').classList.replace('hidden', 'visible');
@@ -19,15 +21,22 @@ document.addEventListener("DOMContentLoaded", () => {
       ctx.fillStyle = "black";
       ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
 
+      var music = new Howl({
+            src: ['./assets/audio/start.mp3']
+      })
       document.getElementById("start-button").addEventListener("click", function (event) {
+
             if (event.currentTarget.classList.contains('visible')) {
+                  if (!music.playing()) {
+                        music.play()
+                  } 
+                  //document.getElementById("splash-page-container").classList.replace('visible', 'hidden');
+
                   document.getElementById("start-button").classList.replace('visible', 'hidden');
                   document.getElementById("title").classList.replace('visible', 'hidden');
                   document.getElementById("sound").classList.replace('visible', 'hidden');
                   document.getElementById("instructions").classList.replace('visible', 'hidden');
-
                   setTimeout(function () {
-                        
                         document.getElementById('sound').classList.add('anchor');
                         document.getElementById('sound').classList.replace('hidden', 'visible');
                   }, 1000);
@@ -39,21 +48,27 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       document.getElementById('sound').addEventListener('click', function (event) {
-            let sound = document.getElementById("audiotag1");
-            if (sound.paused) {
-                  sound.play()
+            
+            if (music.playing()) {
+                  music.stop()
             } else {
-                  sound.pause();
-                  sound.currentTime = 0;
-                  sound.src = sound.src;
+                  music.play()
             }
+            // } let sound = document.getElementById("audiotag1");
+            // if (sound.paused) {
+            //       sound.play()
+            // } else {
+            //       sound.pause();
+            //       sound.currentTime = 0;
+            //       sound.src = sound.src;
+            // }
       })
-      
+
 });
 
 window.addEventListener('resize', () => {
       const canvasEl = document.getElementById("game-canvas");
       canvasEl.width = window.innerWidth;
-      canvasEl.height = window.innerHeight  - window.board_offset;
+      canvasEl.height = window.innerHeight - window.board_offset;
 })
 
